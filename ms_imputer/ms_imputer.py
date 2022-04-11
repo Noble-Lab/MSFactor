ba4044d17e3275c533e1134797638bfa228ace6f
lincoln-harris <ljharris018@gmail.com>
2022-04-11 15:17:45 -0700

    added cross-validation code in ms_imputer, also some adnl utilities functions

--- ms_imputer/ms_imputer.py
+++ ms_imputer/ms_imputer.py
@@ -17,9 +17,6 @@ import ms_imputer.utilities
 @click.option("--output_stem", type=str,
 				help="file stem to use for output file",
 				required=True)
-@click.option("--factors", type=int,
-				help="number of factors to use for reconstruction",
-				required=False)
 @click.option("--learning_rate", type=float,
 				help="the optimizer learning rate", 
 				required=False)
@@ -29,27 +26,48 @@ import ms_imputer.utilities
 def main(
 		csv_path, 
 		output_stem,
-		factors=None, 
 		learning_rate=None, 
-		max_epochs=None
+		max_epochs=None,
 ):
 	"""  
-	Fit an NMF model to the input matrix, impute missing values.
+	Search across a 1D grid of hyperparameters to select the optimal
+	value for n_factors. Then train an NMF model with the optimal
+	value of n_factors, and reconstruct missing values in the input
+	matrix. 
+
+	Arguments
+	---------
+	csv_path : str, posix.Path to the input {peptide, protein} quants
+				matrix. Can be trimmed or raw MaxQuant output. 
+				Required. 
+	output_stem : str, file stem to use for the reconstructed matrix
+				output file. 
+				Required.
+	learning_rate : float, the initial learning rate to use for the 
+				model's (Adam) optimizer.
+				Optional. Default=0.0001.
+	max_epochs : int, the maximum number of training epochs for the model.
+				Optional. Default=3000.
 	"""
-	# Default model configs. Overwritten by commandline args
-	n_factors = 8
-	lr = 0.05
-	max_iters = 3000
+	# Default model configs. These will be overwritten by cmdline args
+	_learning_rate = 0.05
+	_max_epochs = 3000
+
+	# Default model configs. Not overwritable
 	tolerance = 0.0001
 	batch_size = 1000
+	factors_grid = [1,2,4,8,16,32]
+	#factors_grid = [1,2,4]
+	k_folds = len(factors_grid)
 
-	# Overwrite default configs, if specified
-	if factors:
-		n_factors = factors 
+	# for selecting the k-index of the validation split
+	k_remaining = list(range(0, k_folds)) 
+
+	# Overwrite default configs, if specified at cmdline
 	if learning_rate:
-		lr = learning_rate
+		_learning_rate = learning_rate
 	if max_epochs:
-		max_iters = max_epochs
+		_max_epochs = max_epochs
 
 	# trim the input file, if need be
 	trim_bool = ms_imputer.utilities.maxquant_trim(csv_path, output_stem)
@@ -63,32 +81,89 @@ def main(
 	quants_matrix.replace([0, 0.0], np.nan, inplace=True)
 	quants_matrix = np.array(quants_matrix)
 
-	# init model
-	nmf_model = GradNMFImputer(
-					n_rows=quants_matrix.shape[0], 
-					n_cols=quants_matrix.shape[1], 
-					n_factors=n_factors, 
-					stopping_tol=tolerance, 
-					train_batch_size=batch_size, 
-					eval_batch_size=batch_size,
-					n_epochs=max_iters, 
-					loss_func="MSE",
-					optimizer=torch.optim.Adam,
-					optimizer_kwargs={"lr": lr}
+	# get indices, for k-folds cross validation
+	k_fold_indices = ms_imputer.utilities.shuffle_and_split(quants_matrix, k_folds)
+	cross_valid_results = []
+
+	# k-folds cross validation loop. Implements hyperparameter search
+	for k in range(0, k_folds):
+		print("working on fold: ", k)
+		train, val, test, k_remaining = ms_imputer.utilities.get_kfold_sets(
+											quants_matrix, k_fold_indices, 
+											k, k_remaining)
+		n_factors = factors_grid[k]
+
+		# init model
+		nmf_model = GradNMFImputer(
+						n_rows=train.shape[0], 
+						n_cols=train.shape[1], 
+						n_factors=n_factors, 
+						stopping_tol=tolerance, 
+						train_batch_size=batch_size, 
+						eval_batch_size=batch_size,
+						n_epochs=_max_epochs, 
+						loss_func="MSE",
+						optimizer=torch.optim.Adam,
+						optimizer_kwargs={"lr": _learning_rate}
+					)
+		# fit model, get reconstruction
+		recon = nmf_model.fit_transform(train, val)
+
+		# collect errors
+		train_loss = ms_imputer.utilities.mse_func_np(train, recon)
+		val_loss = ms_imputer.utilities.mse_func_np(val, recon)
+		test_loss = ms_imputer.utilities.mse_func_np(test, recon)
+
+		res = {
+			"fold": k,
+			"n_factors": n_factors,
+			"train_error": train_loss,
+			"valid_error": val_loss,
+			"test_error": test_loss,
+		}
+
+		cross_valid_results += [pd.DataFrame(res, index=[0])]
+    
+	# format cross validation results dataframe
+	cross_valid_results = pd.concat(cross_valid_results)
+	cross_valid_results = cross_valid_results.reset_index(drop=True)
+	cross_valid_results.to_csv("cross_valid_results.csv", index=False)
+
+	# select the optimal choice in latent factors
+	val_err_min = np.min(cross_valid_results["valid_error"])
+	min_fold_df = cross_valid_results[cross_valid_results["valid_error"] == val_err_min]
+	optimal_factors = list(min_fold_df["n_factors"])[0]
+
+	# partition
+	train, val, test = ms_imputer.utilities.split(
+							quants_matrix, val_frac=0.1, test_frac=0.01, 
+							min_present=0)
+
+	print("training model with optimal choice in latent factors")
+	# init optimal model
+	nmf_model_opt = GradNMFImputer(
+						n_rows=train.shape[0], 
+						n_cols=train.shape[1], 
+						n_factors=optimal_factors, 
+						stopping_tol=tolerance, 
+						train_batch_size=batch_size, 
+						eval_batch_size=batch_size,
+						n_epochs=_max_epochs, 
+						loss_func="MSE",
+						optimizer=torch.optim.Adam,
+						optimizer_kwargs={"lr": _learning_rate}
 				)
-
 	# fit model, get reconstruction
-	print(" ")
-	print("fitting model")
-	recon = nmf_model.fit_transform(quants_matrix)
+	optimal_recon = nmf_model_opt.fit(train, val)
+	optimal_recon = nmf_model_opt.transform(quants_matrix)
 
-	# write to csv
-	pd.DataFrame(recon).to_csv(
+	# write optimally reconstructed matrix to csv
+	pd.DataFrame(optimal_recon).to_csv(
 					output_stem + 
 					"_reconstructed.csv",
 					index=False
 	)
-	
+	# cleanup
 	try:
 		os.remove(output_stem + "_quants.csv")
 	except FileNotFoundError:
--- ms_imputer/utilities.py
+++ ms_imputer/utilities.py
@@ -5,6 +5,8 @@ ms_imputer utilities
 """
 import pandas as pd
 import numpy as np
+import random
+rng = np.random.default_rng(42) # random seed
 
 def maxquant_trim(maxquant_path, out_stem):
 	"""
@@ -50,4 +52,189 @@ def maxquant_trim(maxquant_path, out_stem):
 
 	# trimmed
 	return 1
+
+
+def get_kfold_sets(matrix, split_indices, k, k_remaining):
+	""" 
+	Given a list of indices and a matrix, generates train, 
+	validation and test sets. 
+
+	Parameters
+	----------
+	matrix : np.ndarray, the input matrix
+	split_indices : np.ndarray, a list of lists, outputted from 
+	                shuffle_and_split()
+	k : int, the fold
+	k_remaining : array of ints, the remaining indices that have not
+			yet been selected for cross validation
+
+	Returns
+	-------
+	train_set, valid_set, test_set: np.ndarray, the training, 
+	                                validation and test sets
+	k_remaining : array of ints, with the selected k removed
+	"""
+	# init test and validation sets
+	test_set = np.full(matrix.shape, np.nan)
+	valid_set = np.full(matrix.shape, np.nan)
+
+	# get index for validation set such that j != k
+	j = random.choice([elm for elm in k_remaining if elm != k])
+	# so that we never repeat j
+	k_remaining.remove(j)
+
+	test_indices = split_indices[k]
+	valid_indices = split_indices[j]
+
+	# unpackage zipped array, get tuples
+	test_rows, test_cols = zip(*test_indices)
+	valid_rows, valid_cols = zip(*valid_indices)
+    
+	# add values to validation and test sets
+	test_set[test_rows, test_cols] = matrix[test_rows, test_cols]
+	valid_set[valid_rows, valid_cols] = matrix[valid_rows, valid_cols]
+
+	# get validation and test masks
+	valid_mask = np.isnan(valid_set)
+	test_mask = np.isnan(test_set)
+
+	# init train set, set validation and test masks to nan
+	train_set = matrix.copy()
+	train_set[~valid_mask] = np.nan
+	train_set[~test_mask] = np.nan
+
+	return train_set, valid_set, test_set, k_remaining
+
+
+def mse_func_np(x_mat, y_mat):
+	"""
+	Calculate the MSE for two matricies with missing values. Each
+	matrix can contain MVs, in the form of np.nans
+
+	Parameters
+	----------
+	x_mat : np.ndarray, the first matrix 
+	y_mat : np.ndarray, the second matrix
+
+	Returns
+	-------
+	float, the mean squared error between values present 
+	        across both matrices
+	"""
+	x_rav = x_mat.ravel()
+	y_rav = y_mat.ravel()
+	missing = np.isnan(x_rav) | np.isnan(y_rav)
+	mse = np.sum((x_rav[~missing] - y_rav[~missing])**2)
+
+	if (np.sum(~missing) == 0):
+		print("Warning: Computing MSE from all missing values.")
+		return 0
+	return mse / np.sum(~missing)
+
+
+def shuffle_and_split(matrix, k_folds):
+	""" 
+	Shuffle the indices from an input matrix, and then split 
+	into k_folds equal pieces. 
+
+	Parameters
+	----------
+	matrix: np.ndarray, the input matrix
+	k_folds: int, the number of folds to split into
+
+	Returns
+	-------
+	split_indices: np.ndarray, a list of list containing 
+	        randomly selected indices for each of k splits
+	"""
+	# get indices from matrix, shuffle
+	indices = np.vstack(np.nonzero(~np.isnan(matrix)))
+	rng.shuffle(indices, axis=1)
+
+	# zip, into array 
+	indices_shuffled_zip = np.array(list(zip(indices[0],indices[1])))
+
+	# split shuffled indices into k_folds equal pieces
+	split_indices = np.array_split(indices_shuffled_zip, k_folds)
+
+	return split_indices
+
+
+def split(matrix, val_frac=0.1, test_frac=0.1, min_present=5, 
+            random_state=42):
+    """
+    Split a data matrix into training, validation, test sets.
+
+    Note that the fractions of data in the validation and tests 
+    sets is only approximate due to the need to drop rows with 
+    too much missing data.
+
+    Parameters
+    ----------
+    matrix : array-like
+        The data matrix to split.
+    val_frac : float, optional
+        The fraction of data to assign to the validation set.
+    test_frac : float, optional
+        The fraction of data to assign to the test set.
+    min_present : int, optional
+        The minimum number of non-missing values required in each 
+        row of the training set.
+    random_state : int or numpy.random.Generator
+        The random state for reproducibility.
+
+    Returns
+    -------
+    train_set : numpy.ndarray
+        The training set.
+    val_set : numpy.ndarray
+        The validation set, where other values are NaNs.
+    test_set : numpy.ndarray
+        The test set, where other values are NaNs.
+    """
+    rng = np.random.default_rng(random_state)
+    if val_frac + test_frac > 1:
+        raise ValueError("'val_frac' and 'test_frac' cannot sum to more than 1.")
+
+    # Prepare the matrix:
+    matrix = np.array(matrix).astype(float)
+    matrix[matrix == 0] = np.nan
+    num_present = np.sum(~np.isnan(matrix), axis=1)
+    discard = num_present < min_present
+    num_discard = discard.sum()
+
+    matrix = np.delete(matrix, discard, axis=0)
+
+    # Assign splits:
+    indices = np.vstack(np.nonzero(~np.isnan(matrix)))
+    rng.shuffle(indices, axis=1)
+
+    n_val = int(indices.shape[1] * val_frac)
+    n_test = int(indices.shape[1] * test_frac)
+    n_train = indices.shape[1] - n_val - n_test
+
+    train_idx = tuple(indices[:, :n_train])
+    val_idx = tuple(indices[:, n_train:(n_train + n_val)])
+    test_idx = tuple(indices[:, -n_test:])
+
+    train_set = np.full(matrix.shape, np.nan)
+    val_set = np.full(matrix.shape, np.nan)
+    test_set = np.full(matrix.shape, np.nan)
+
+    train_set[train_idx] = matrix[train_idx]
+    val_set[val_idx] = matrix[val_idx]
+    test_set[test_idx] = matrix[test_idx]
+
+    # Remove Proteins with too many missing values:
+    num_present = np.sum(~np.isnan(train_set), axis=1)
+    discard = num_present < min_present
+    num_discard = discard.sum()
+
+    train_set = np.delete(train_set, discard, axis=0)
+    val_set = np.delete(val_set, discard, axis=0)
+    test_set = np.delete(test_set, discard, axis=0)
+
+    return train_set, val_set, test_set
+
+
 
