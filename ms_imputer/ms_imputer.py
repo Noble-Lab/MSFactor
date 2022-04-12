ee24df77aed7bab1613e7cdfe345f4d25d9b8dfc
lincoln-harris <ljharris018@gmail.com>
2022-04-12 14:56:20 -0700

    adding nested cross validation procedure

--- ms_imputer/ms_imputer.py
+++ ms_imputer/ms_imputer.py
@@ -12,83 +12,13 @@ import os
 from ms_imputer.models.linear import GradNMFImputer
 import ms_imputer.utilities
 
-def estimate_confidence(
-					matrix, 
-					folds, 
-					n_factors, 
-					tol, 
-					b_size, 
-					n_epochs, 
-					lr,
-):
-	""" 
-	Cross validation-based confidence estimation. For each of
-	k-folds, reconstruct the matrix. Then, collect standard 
-	deviation for every element in the matrix, across all of
-	k reconstructions. 
-	
-	Parameters
-	----------
-	matrix : array-like, 2D, the input matrix 
-	folds : int, the number of folds
-	n_factors : int, the number of factors to use training NMF model
-	tol : float, the early-stopping tolerance for the NMF model
-	b_size : int, the model batch size
-	n_epochs : int, the maximum number of model training epochs
-	lr : float, the model's Adam optimizer initial learning rate
-
-	Returns
-	--------
-	stds_mat : np.array, standard deviations calculated across each fold, 
-				for each matrix element that was originally missing
-	"""
-	# get indices, for k-folds cross validation
-	k_fold_indices = ms_imputer.utilities.shuffle_and_split(matrix, folds)
-	
-	results = []
-
-	# k-folds cross validation loop. Implements hyperparameter search
-	for k in range(0, folds):
-		print("working on fold: ", k)
-		train, val = ms_imputer.utilities.get_kfold_sets(
-													matrix, 
-													k_fold_indices, 
-													k,
-		)
-		# init model
-		nmf_model = GradNMFImputer(
-						n_rows=train.shape[0], 
-						n_cols=train.shape[1], 
-						n_factors=n_factors, 
-						stopping_tol=tol, 
-						train_batch_size=b_size, 
-						eval_batch_size=b_size,
-						n_epochs=n_epochs, 
-						loss_func="MSE",
-						optimizer=torch.optim.Adam,
-						optimizer_kwargs={"lr": lr},
-		)
-		# fit model, get reconstruction
-		recon = nmf_model.fit_transform(train, val)
-		results.append(recon.flatten())
-
-	# get standard deviations across each matrix element
-	results = np.array(results)
-	stds = np.std(results, axis=0)
-	
-	# reshape, into original matrix shape
-	stds_mat = stds.reshape(matrix.shape)
-
-	# want to return stds for only the original nans
-	orig_nans = np.isnan(matrix)
-	stds_mat[~orig_nans] = np.nan
-
-	return stds_mat
-
 def cross_validate(matrix, folds, grid, tol, b_size, n_epochs, lr):
 	""" 
-	Run cross validation, by training an NMF model for a given matrix
-	for each of a set of hyperparameters (n_factors)
+	Run nested cross validation for an input matrix, a given
+	hyperparameter grid and a given value of k. For each of k folds, 
+	partition into train and validate, train NMF models across 
+	every hparam. Record train and validation errors. Return a 
+	dataframe of per-hyperparameter averages, across each of k folds
 	
 	Parameters
 	----------
@@ -102,58 +32,73 @@ def cross_validate(matrix, folds, grid, tol, b_size, n_epochs, lr):
 
 	Returns
 	--------
-	results : pd.DataFrame, tabular output of cross validation
+	averages_df : pd.DataFrame, tabular output of cross validation
 	"""
 	# get indices, for k-folds cross validation
 	k_fold_indices = ms_imputer.utilities.shuffle_and_split(matrix, folds)
 	
 	results = []
 
-	# k-folds cross validation loop. Implements hyperparameter search
+	# k-folds cross validation loop
 	for k in range(0, folds):
 		print("working on fold: ", k)
+		# partition
 		train, val = ms_imputer.utilities.get_kfold_sets(
-													matrix, 
-													k_fold_indices, 
-													k,
+											matrix, 
+											k_fold_indices, 
+											k,
 		)
-		n_factors = grid[k]
+		# hyperparameter search
+		for n_factors in grid:
+			# init model
+			nmf_model = GradNMFImputer(
+							n_rows=train.shape[0], 
+							n_cols=train.shape[1], 
+							n_factors=n_factors, 
+							stopping_tol=tol, 
+							train_batch_size=b_size, 
+							eval_batch_size=b_size,
+							n_epochs=n_epochs, 
+							loss_func="MSE",
+							optimizer=torch.optim.Adam,
+							optimizer_kwargs={"lr": lr},
+			)
+			# fit model, get reconstruction
+			recon = nmf_model.fit_transform(train, val)
 
-		# init model
-		nmf_model = GradNMFImputer(
-						n_rows=train.shape[0], 
-						n_cols=train.shape[1], 
-						n_factors=n_factors, 
-						stopping_tol=tol, 
-						train_batch_size=b_size, 
-						eval_batch_size=b_size,
-						n_epochs=n_epochs, 
-						loss_func="MSE",
-						optimizer=torch.optim.Adam,
-						optimizer_kwargs={"lr": lr},
-		)
-		# fit model, get reconstruction
-		recon = nmf_model.fit_transform(train, val)
+			# collect errors
+			train_loss = ms_imputer.utilities.mse_func_np(train, recon)
+			val_loss = ms_imputer.utilities.mse_func_np(val, recon)
 
-		# collect errors
-		train_loss = ms_imputer.utilities.mse_func_np(train, recon)
-		val_loss = ms_imputer.utilities.mse_func_np(val, recon)
+			res = {
+				"fold": k,
+				"n_factors": n_factors,
+				"train_error": train_loss,
+				"valid_error": val_loss,
+			}
 
-		res = {
-			"fold": k,
-			"n_factors": n_factors,
-			"train_error": train_loss,
-			"valid_error": val_loss,
-		}
-
-		results += [pd.DataFrame(res, index=[0])]
+			results += [pd.DataFrame(res, index=[0])]
     
-	# format cross validation results dataframe
+	# format nested cross validation results dataframe
 	results = pd.concat(results)
 	results = results.reset_index(drop=True)
-	#results.to_csv("cross_valid_results.csv", index=False)
 
-	return results
+	averages_df = pd.DataFrame(columns=["n_factors", \
+								"train_error", "valid_error"])
+	# average results across each of k folds
+	for factor in grid:
+		sub_df = results[results["n_factors"] == factor]
+		train_err_mean = np.mean(sub_df["train_error"])
+		valid_err_mean = np.mean(sub_df["valid_error"])
+
+		averages_df = averages_df.append({
+	    							'n_factors' : factor, 
+	    							'train_error' : train_err_mean, 
+	    							'valid_error' : valid_err_mean},
+	    							ignore_index=True
+		)
+
+	return averages_df
 
 @click.command()
 @click.option("--csv_path", type=str, 
@@ -167,18 +112,17 @@ def cross_validate(matrix, folds, grid, tol, b_size, n_epochs, lr):
 				required=False, default=0.05)
 @click.option("--max_epochs", type=int,
 				help="max number of training epochs", 
-				required=False, default=3000)
-@click.option("--report_confidence", type=bool,
-				help="report confidence intervals associated \
-				with each prediction?", 
-				required=False, default=False)			
+				required=False, default=3000)	
+@click.option("--k_folds", type=int,
+				help="number of cross validation folds", 
+				required=False, default=10)		
 
 def main(
 		csv_path, 
 		output_stem,
 		learning_rate, 
 		max_epochs,
-		report_confidence,
+		k_folds,
 ):
 	"""  
 	Search across a 1D grid of hyperparameters to select the optimal
@@ -199,16 +143,12 @@ def main(
 				Optional. Default=0.05.
 	max_epochs : int, the maximum number of training epochs for the model.
 				Optional. Default=3000.
-	report_confidence : bool, report the confidence interval associated 
-				with each prediction. Requires an additional round
-				of cross validation.
-				Optional. Default=False
+	k_folds : int, k, for k-folds cross validation
 	"""
 	# Default model configs. Not overwritable
 	tolerance = 0.0001
 	batch_size = 1000
 	factors_grid = [1,2,4,8,16,32]
-	k_folds = len(factors_grid)
 
 	# trim the input file, if need be
 	trim_bool = ms_imputer.utilities.maxquant_trim(csv_path, output_stem)
@@ -245,13 +185,12 @@ def main(
 										test_frac=0.01, 
 										min_present=0,
 	)
-
 	print("training model with optimal choice in latent factors")
 	# init optimal model
 	nmf_model_opt = GradNMFImputer(
 						n_rows=train.shape[0], 
 						n_cols=train.shape[1], 
-						n_factors=optimal_factors, 
+						n_factors=int(optimal_factors), 
 						stopping_tol=tolerance, 
 						train_batch_size=batch_size, 
 						eval_batch_size=batch_size,
@@ -275,20 +214,6 @@ def main(
 		os.remove(output_stem + "_quants.csv")
 	except FileNotFoundError:
 		pass
-
-	if report_confidence:
-		print("running cross validation-based confidence estimation")
-		matrix_stds = estimate_confidence(
-							quants_matrix, 
-							k_folds, 
-							optimal_factors,
-							tolerance,
-							batch_size,
-							max_epochs,
-							learning_rate,
-		)
-		matrix_stds = pd.DataFrame(matrix_stds)
-		matrix_stds.to_csv("imputed_matrix_stds.csv", index=None)
 
 	print("Done!")
 	print(" ")
